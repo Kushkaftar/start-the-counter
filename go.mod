afa3472c4e4c9cc3504e8a3d4b4a711a1b65d936
kushkaftar <kushkaftar@gmail.com>
2019-11-23 11:11:55 +0300

    add migration

--- go.mod
+++ go.mod
@@ -2,4 +2,13 @@ module github.com/Kushkaftar/start-the-counter
 
 go 1.13
 
-require github.com/BurntSushi/toml v0.3.1
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/golang-migrate/migrate v3.5.4+incompatible // indirect
+	github.com/golang-migrate/migrate/v4 v4.7.0 // indirect
+	github.com/gorilla/mux v1.7.3
+	github.com/jmoiron/sqlx v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	github.com/stretchr/testify v1.4.0 // indirect
+)
--- iternal/app/apiserver/apiserver.go
+++ iternal/app/apiserver/apiserver.go
@@ -1,18 +1,80 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
+import "github.com/gorilla/mux"
+
+import "net/http"
+
+import "io"
+
+import "github.com/Kushkaftar/start-the-counter/iternal/app/store"
+
 // APIServer ...
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
+	router *mux.Router
+	store *store.Store
 }
 
 // New ...
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
 // Start ...
-func (a *APIServer) Start() error {
+func (s *APIServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.configureRouter()
+
+	if err := s.configureStore(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return http.ListenAndServe(s.config.BindAddr, s.router)
+}
+
+// configureLogger ...
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
 	return nil
+}
+
+// configureRouter ...
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/", s.index())
+}
+
+// configureStore ...
+func (s *APIServer) configureStore() error {
+	st := store.New(s.config.Store)
+	if err := st.Open(); err != nil {
+		return err
+	}
+
+	s.store = st
+
+	return nil
+}
+
+// index ...
+func (s *APIServer) index() http.HandlerFunc {
+	return func (w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<h1>www</h1>")
+	}
 }
